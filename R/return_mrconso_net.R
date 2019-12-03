07fd6752485e9765a523ca108dcd2f3f93604080
patelm9 <patelm9@mskcc.org>
2019-12-03 13:58:49 -0500

    bug fix return_mrconso_net

--- R/return_mrconso_net.R
+++ R/return_mrconso_net.R
@@ -22,7 +22,7 @@ return_mrconso_net <-
                  list_by_cui = TRUE) {
 
                 lookup_fn <- path_to_lookup
-                files_list <- list.files(path_to_umls_nets, full.names = TRUE)
+                files_list <- list.files(path_to_umls_net_dir, full.names = TRUE)
                 lookup <- readr::read_csv(lookup_fn, col_types = cols(.default = "c"))
 
                 phrase_00 <- phrase
