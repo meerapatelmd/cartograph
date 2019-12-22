75a84abd65cabf0eb70097ecba82b9d2ddde6a70
patelm9 <patelm9@mskcc.org>
2019-12-22 14:20:58 -0500

    +: umls_glossary dataset

--- R/data.R
+++ R/data.R
@@ -3,6 +3,8 @@
 #' A list of 3 dataframes that provide descriptions of the TTY and STT variable abbreviations from the 2019 version of UMLS Metathesaurus
 #' 
 #' @format list of 3 dataframes
+#' @name umls_glossary
+#' @usage data(umls_glossary)
 #' @source \url{https://www.nlm.nih.gov/research/umls/knowledge_sources/metathesaurus/release/abbreviations.html#TTYC}
 #' 
-
+NULL
