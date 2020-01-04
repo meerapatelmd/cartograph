90d49a59def1a4c2c1f21542639224670f0a9467
Meera Patel <patelm9@mskcc.org>
2020-01-03 20:08:29 -0500

    +: add stop_and_enter feature

--- R/add_to_key.R
+++ R/add_to_key.R
@@ -1,6 +1,7 @@
 #' Adds a new key data to the existing key as indicated by the path_to_key
 #' @importFrom mirCat append_csv
 #' @importFrom typewriteR tell_me
+#' @importFrom typewriteR stop_and_enter
 #' @import dplyr
 #' @importFrom stringr str_replace_all
 #' @importFrom mirCat get_timestamp
@@ -14,12 +15,21 @@ add_to_key <-
                 stopifnot(length(key_type) == 1)
 
                 if (key_type == "umls") {
+
+                        typewriteR::tell_me("Will now append the input after updating the timestamp and overwriting MSK_CONCEPT_ID using the CUI column.")
+                        typewriteR::stop_and_enter()
+
+
                         mirCat::append_csv(csv_fn = path_to_key,
                                            dataframe = dataframe %>%
                                                    dplyr::mutate(KEY_TIMESTAMP = mirCat::get_timestamp()) %>%
                                                    dplyr::mutate(MSK_CONCEPT_ID = stringr::str_replace_all(CUI, "^C", "MSK"))
                                            )
                 } else if (key_type == "omop") {
+
+                        typewriteR::tell_me("Will now append the input after updating the timestamp")
+                        typewriteR::stop_and_enter()
+
                         mirCat::append_csv(csv_fn = path_to_key,
                                            dataframe = dataframe %>%
                                                    dplyr::mutate(KEY_TIMESTAMP = mirCat::get_timestamp()))
