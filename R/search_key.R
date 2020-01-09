7291a7a0ef9546c8ee346197d470d1fb518e725c
Meera Patel <patelm9@mskcc.org>
2020-01-08 19:23:59 -0500

    +: search_key function

--- R/search_key.R
+++ R/search_key.R
@@ -1,17 +1,22 @@
-#'Search key for keyword
-#'@import dplyr
-#'@export
-
+#' Search key for a term
+#' @param path_to_key path to key file
+#' @param string string to search
+#' @importFrom readr read_csv
+#' @import dplyr
+#' @export
+#'
 
 search_key <-
         function(path_to_key,
-                 keyword) {
-                
-                key <- read_key(path_to_key = path_to_key,
-                                project_alias = "all")
+                 string) {
 
-                return(
-                        key %>%
-                                dplyr::filter_all(any_vars(grepl(keyword, ., ignore.case = TRUE) == TRUE))
-                )
+                key_data <-
+                        readr::read_csv(path_to_key,
+                                        col_types = cols(.default = "c"))
+
+                output <-
+                        key_data %>%
+                        dplyr::filter_all(any_vars(grepl(string, ., ignore.case = TRUE)))
+
+                return(output)
         }
