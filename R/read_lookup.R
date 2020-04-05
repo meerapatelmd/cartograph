aa3b601bed8199859dee5d2a8b3972dd2d0dd1a7
Meera Patel <patelm9@mskcc.org>
2020-04-05 02:39:11 -0400

    remove stale functions

--- R/read_lookup.R
+++
@@ -1,9 +0,0 @@
-#' Reads lookup
-#' @import readr
-#' @export
-
-read_lookup <-
-        function(path_to_lookup) {
-                
-                return(readr::read_csv(path_to_lookup, col_types = readr::cols(.default = "c")))
-        }
